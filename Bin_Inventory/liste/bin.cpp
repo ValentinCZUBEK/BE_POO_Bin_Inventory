e14aae8ab7a97fac9d1fd0b2cee7381ccf61feda
ValentinCZUBEK <valczub@outlook.fr>
2026-01-09 10:36:18 +0100

    finished classes item bin and caracteristics

---
+++ Bin_Inventory/liste/bin.cpp
@@ -0,0 +1,13 @@
+#include "bin.h"
+
+
+bin::bin(int nb_sl, struct coordinates *aff_sl): nb_affected_slots(nb_sl), affected_slots(aff_sl) {
+
+}
+
+bin::~bin(){
+
+}
+
+
+
---
+++ Bin_Inventory/liste/caracteristics.cpp
@@ -0,0 +1,9 @@
+#include "caracteristics.h"
+
+caracteristics::caracteristics(string nm, string vl, int qty): name(nm), value(vl), quantity(qty) {
+    
+}
+
+caracteristics::~caracteristics(){
+    
+}

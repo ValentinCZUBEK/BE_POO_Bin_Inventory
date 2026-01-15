a5ac35dc3264d18e8d5152a3a85fb10446683008
ValentinCZUBEK <valczub@outlook.fr>
2026-01-16 00:33:27 +0100

    inclusions fichiers cpp pour arduino

--- Bin_Inventory/Bin_Inventory.ino
+++ Bin_Inventory/Bin_Inventory.ino
@@ -1,4 +1,15 @@
 #include "Application.h"
+#include "Application.cpp"
+#include "Hardware/Grid.cpp"
+#include "Hardware/Capteur.cpp"
+#include "Hardware/Led.cpp"
+#include "liste/bin.cpp"
+#include "liste/caracteristics.cpp"
+#include "liste/cellule.cpp"
+#include "liste/file.cpp"
+#include "liste/item.cpp"
+#include "Manager/DetectionManager.cpp"
+#include "Manager/itemsManager.cpp"
 
 Application myApplication;
 

287863b5dd51cc2c393402c56ee393eecfd3c647
ValentinCZUBEK <valczub@outlook.fr>
2026-01-16 15:28:35 +0100

    commentaires dossier manager

--- Bin_Inventory/Manager/DetectionManager.h
+++ Bin_Inventory/Manager/DetectionManager.h
@@ -7,17 +7,23 @@
 
 class DetectionManager {
 private:
-    bool etatPrecedentCapteurs[TAILLE][TAILLE];
-    bool changementAjoutDetecte;
-    bool changementSuppressionDetecte;
-    unsigned long lastDetectionTime;
-    static const unsigned long DELAI_DETECTION = 500;
+    bool etatPrecedentCapteurs[TAILLE][TAILLE]; // État précédent des capteurs pour détection des changements
+    bool changementAjoutDetecte;    // Drapeau si un ajout a été détecté
+    bool changementSuppressionDetecte;  // Drapeau si une suppression a été détectée
+    unsigned long lastDetectionTime;    // Temps de la dernière détection, qu'on utilisera pour trouver le temps écoulé
+    static const unsigned long DELAI_DETECTION = 500;   // Délai "anti-rebond" en millisecondes
 
 public:
-    DetectionManager();
-    void detecterChangement(Grid4x4 *grille);
-    void gererDetection(Grid4x4 *grille, itemManager *manager);
+    DetectionManager(); 
+
+    void detecterChangement(Grid4x4 *grille);   
+    // Détecte les changements dans l'état des capteurs en lisant la grille
+
+    void gererDetection(Grid4x4 *grille, itemManager *manager); 
+    // Gère les actions à effectuer en fonction des changements détectés (ajout ou suppression, dans le cas de l'ajout on demande les infos via la console série)
+
     void setInitialState(Grid4x4 *grille);
+    // Initialise l'état précédent des capteurs avec l'état actuel de la grille (pour éviter les fausses détections au démarrage avec des valeurs aléatoires)
 };
 
 #endif

04cc45d631076832ce459951854bb15a60b1c9cc
ValentinCZUBEK <valczub@outlook.fr>
2026-01-16 14:48:11 +0100

    plus de commentaires

--- Bin_Inventory/Manager/itemsManager.h
+++ Bin_Inventory/Manager/itemsManager.h
@@ -9,8 +9,6 @@
 #include "Arduino.h"
 
 class itemManager{
-    protected: 
-    //Grid4x4 grid;
 
     private:
     file<item*> ListeItems;
@@ -21,13 +19,28 @@ class itemManager{
     ~itemManager();
 
     void add(int nb_slots, struct coordinates *affected_slots, String name, String value, int quantity);
+    // Ajout d'un item à la liste en lui passant les caractéristiques et spécifications de la bin
+    
     void remove(int Id);
+    // Suppression d'un item de la liste via son identifiant
+
     void dell();
+    // Suppression du dernier item de la liste
+
     void gridRemoveCells(int nb_slots, struct coordinates *affected_slots);
+    // Suppression des items dont au moins un slot est affecté et doit être supprimé
+
     void displayItems();
+    // Affichage de tous les items dans la console série
+
     void modifyQuantity();
-    void enlightItem(Grid4x4* grid);
+    // Modification de la quantité d'un item via la console série
+
+    void enlightItem(Grid4x4* grid);   
+    // Allumer les LEDs des slots où se trouve la bin sélectionnée (item)
+
     void actionChoice(Grid4x4* grid);
+    // Choix de l'action à effectuer via la console série (modifier quantité ou allumer LEDs)
 };
 
 #endif
